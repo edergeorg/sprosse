72f88ed86e1562dd6d3f5970a9d3ae435627e61b
Georg Eder <georgeder159@gmail.com>
2026-05-05 22:24:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Sprosse Service Worker v0.9.4
-const CACHE = 'sprosse-v0.9.4';
+// Sprosse Service Worker v0.9.5
+const CACHE = 'sprosse-v0.9.5';
 
 self.addEventListener('install', function(e) {
   self.skipWaiting();
